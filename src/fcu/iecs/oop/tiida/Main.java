5e68fa65bc5a9c8a9a7ac96eec2efe61a8e87ff8
louie <louie@asus>
2016-10-21 14:47:10 +0800

    HW4

--- src/fcu/iecs/oop/tiida/Main.java
+++ src/fcu/iecs/oop/tiida/Main.java
@@ -1,9 +1,18 @@
 package fcu.iecs.oop.tiida;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num,i;
+		NissanTiida nissan=new NissanTiida();
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Please enter a number");
+		num=scanner.nextInt();
+		for(i=1;i<=num;i++) {
+			nissan.Tiida(i);
+			System.out.printf("\n");
+		}
+		scanner.close();		
 
 	}
 
--- src/fcu/iecs/oop/tiida/NissanTiida.java
+++ src/fcu/iecs/oop/tiida/NissanTiida.java
@@ -1,5 +1,14 @@
 package fcu.iecs.oop.tiida;
 
 public class NissanTiida {
+	public void Tiida(int num) {
+		int i=0,j=0;
+		for(i=0;i<num;i++) {
+			for(j=0;j<num;j++) {
+				System.out.printf("*");
+			}
+			System.out.printf("\n");
+		}
+	}	
 
 }
